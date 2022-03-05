a6296cd6bf6627f1bf5df58dab01103dc655b4b9
Seth Perna <gseth26@gmail.com>
2022-03-04 19:27:24 -0500

    create forcast service and spec, hitting API as expected

---
+++ app/services/forecast_service.rb
@@ -0,0 +1,15 @@
+class ForecastService
+  def self.connection
+    url = "http://www.mapquestapi.com/geocoding/v1/address?"
+    Faraday.new(url: url) do |faraday|
+      faraday.params['key'] = ENV['map_api_key']
+    end
+  end
+
+  def self.get_coordinates(location)
+    response = connection.get do |faraday|
+      faraday.params['location'] = location
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
---
+++ spec/services/forecast_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Service' do
+  it 'returns a faraday response', :vcr do
+      connection = ForecastService.connection
+      expect(connection).to be_a(Faraday::Connection)
+  end
+
+  it 'returns longitude and lattitude', :vcr do
+    get_location = ForecastService.get_coordinates("Denver,CO")
+    expect(get_location).to be_a Hash
+    expect(get_location[:results]).to be_a Array
+    expect(get_location[:results][0][:locations][0][:latLng]).to have_key(:lat)
+    expect(get_location[:results][0][:locations][0][:latLng]).to have_key(:lng)
+  end
+end
