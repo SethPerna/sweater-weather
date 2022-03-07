2f27d323cdb995f17e4f17a95a88fbabed7ec335
Seth Perna <gseth26@gmail.com>
2022-03-07 12:03:28 -0500

    I keep forgetting to commit, all functionality of serializer complete, beefing up testing.

---
+++ app/serializers/books_serializer.rb
@@ -0,0 +1,28 @@
+class BooksSerializer
+  def self.book_response(forecast, books, location, quantity)
+    hash = {
+      "data":
+      {
+        "id": nil,
+        "type": "books",
+        "attributes":
+        {
+          "destination": "#{location}",
+          "forecast":
+          {
+            "summary": forecast[:current][:weather][0][:description],
+            "temperature": forecast[:current][:temp]
+          },
+          "total_books_found": books[:numFound],
+          "books": books[:docs].shift(quantity).map do |book|
+            {
+              "isbn": book[:isbn],
+              "title": book[:title],
+              "publisher": book[:publisher],
+            }
+          end
+        }
+      }
+    }
+  end
+end
